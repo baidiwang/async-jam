ef6f9f90e5933cadf340ce22f7acd83dbc866439
Yohan Guyomard <yohang@mit.edu>
2025-11-15 16:56:26 -0800

    Add jukebox placement controller and state machine states

--- Assets/Scripts/Jukebox.ts
+++ Assets/Scripts/Jukebox.ts
@@ -1,10 +1,44 @@
 import { AnchorComponent } from "Spatial Anchors.lspkg/AnchorComponent";
 import { AudioRecording } from "./Audio";
+import { Instrument } from "./Instrument";
+
+/**
+ * State machine for a user interaction with the jukebox.
+ */
+enum State {
+    /**
+     * Initial state when the jukebox exists but has been interacted with. The audio
+     * is still playing but at a low volume
+     */
+    Idle,
+    /**
+     * State when the user is in close proximity of the jukebox and looking at it. The
+     * audio is still playing at max volume, and three instrument bubbles are presented
+     */
+    Focused,
+    /**
+     * State after the user has clicked on an instrument bubble. The audio is still playing
+     * at max volume and a virtual instrument has spawned in, but it is *not* recording
+     */
+    InstrumentSelected,
+    /**
+     * State when user has pressed record. Loop is still playing at mid-high volume (so the
+     * user can hear themselves) and the virtual instrument is present and recording for one
+     * loop.
+     */
+    Recording,
+    /**
+     * State after user has recorded. The loop is playing at max volume with their track mixed-in.
+     */
+    RecordingDone,
+}
 
 @component
 export class Jukebox extends BaseScriptComponent {    
     private _anchorComponent?: AnchorComponent;
     private _audioRecording: AudioRecording;
+    private _instrumentOrder: Instrument[];
+    private _instrumentIndex: number;
     
     onAwake() {
         this._anchorComponent = this.sceneObject.getComponent(AnchorComponent.getTypeName());
@@ -28,7 +62,22 @@ export class Jukebox extends BaseScriptComponent {
     }
 
     /**
-     * Add a music track to the current mix of instruments
+     * Get the instrument that should be next added to this jukebox
+     */
+    get currentInstrument() {
+        return this._instrumentOrder[this._instrumentIndex % this._instrumentOrder.length];
+    }
+
+    /**
+     * Get the number of tracks added so far
+     */
+    get numTracks() {
+        return this._instrumentIndex;
+    }
+
+    /**
+     * Add a music track to the current mix of instruments. This changes currentInstrument to
+     * the next instrument
      */
     addTrack(audio: AudioRecording) {
         // TODO
@@ -43,7 +92,8 @@ export class Jukebox extends BaseScriptComponent {
 
     /**
      * Load this jukebox's data from persistent storage. The anchor component should have
-     * an anchor by the time this is called
+     * an anchor by the time this is called. If the anchor isn't found in storage, it will
+     * initialize this jukebox anew
      */
     load() {
         // TODO
--- Assets/Scripts/JukeboxPlacementController.ts
+++ Assets/Scripts/JukeboxPlacementController.ts
@@ -2,19 +2,15 @@ import { Anchor } from "Spatial Anchors.lspkg/Anchor";
 import { AnchorComponent } from "Spatial Anchors.lspkg/AnchorComponent";
 import { AnchorModule } from "Spatial Anchors.lspkg/AnchorModule";
 import { AnchorSession, AnchorSessionOptions } from "Spatial Anchors.lspkg/AnchorSession";
-import { PinchButton } from "SpectaclesInteractionKit.lspkg/Components/UI/PinchButton/PinchButton";
-import SIK from "SpectaclesInteractionKit.lspkg/SIK";
+import { Jukebox } from "./Jukebox";
 
 @component
-export class NewScript extends BaseScriptComponent {
+export class JukeboxPlacementController extends BaseScriptComponent {
     @input anchorModule: AnchorModule;
-    @input pinchButton: PinchButton;
-
-    @input camera: SceneObject;
-    @input prefab: ObjectPrefab;
-
+    @input jukeboxPrefab: ObjectPrefab;
+    
     private anchorSession?: AnchorSession;
-
+    
     async onAwake() {
         this.createEvent("OnStartEvent").bind(() => {
             this.onStart();
@@ -22,33 +18,15 @@ export class NewScript extends BaseScriptComponent {
     }
 
     private async onStart() {
-        // Button just creates an anchor in front of the user
-        this.pinchButton.onButtonPinched.add(() => {
-            const anchorPos = this.camera
-                .getTransform()
-                .getWorldTransform()
-                .mult(mat4.fromTranslation(new vec3(0, 0, -5)));
-            
-            this.createAnchor(anchorPos);
-        });
-
-        // Hands create an anchor wherever the pinch happened
-        const hands = SIK.HandInputData;
-        for (const hand of [hands.getHand("left"), hands.getHand("right")]) {
-            hand.onPinchDown.add(() => {
-                this.createAnchor(mat4.fromTranslation(hand.indexTip.position));
-            });
-        }
-
         const anchorSessionOptions = new AnchorSessionOptions();
+        
         anchorSessionOptions.scanForWorldAnchors = true;
-
         this.anchorSession = await this.anchorModule.openSession(anchorSessionOptions);
         this.anchorSession.onAnchorNearby.add(this.onAnchorNearby.bind(this));
     }
 
     private onAnchorNearby(anchor: Anchor) {
-        
+
     }
 
     private async createAnchor(worldTransform: mat4) {
@@ -63,10 +41,13 @@ export class NewScript extends BaseScriptComponent {
     }
 
     private attachNewObjectToAnchor(anchor: Anchor) {
-        const obj = this.prefab.instantiate(this.getSceneObject());
+        const obj = this.jukeboxPrefab.instantiate(this.getSceneObject());
         obj.setParent(this.getSceneObject());
 
-        const anchorComponent = obj.createComponent(AnchorComponent.getTypeName()) as AnchorComponent;
+        const anchorComponent = obj.getComponent(AnchorComponent.getTypeName());
+        const jukeboxComponent = obj.getComponent(Jukebox.getTypeName());
+
         anchorComponent.anchor = anchor;
+        jukeboxComponent.load();
     }
 }
